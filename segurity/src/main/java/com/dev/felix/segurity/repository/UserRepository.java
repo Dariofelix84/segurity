49a1e3505b4ebc8ddc57f3ff07021cd3234056f5
Dariofelix84 <dariofa69@gmail.com>
2025-12-09 19:32:00 -0300

    Creation UserRepository

---
+++ segurity/src/main/java/com/dev/felix/segurity/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.dev.felix.segurity.repository;
+
+import com.dev.felix.segurity.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<UserDetails> findUserByEmail(String username);
+}
