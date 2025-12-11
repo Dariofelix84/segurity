a3ae5ff734a31ee1b38c3ee873cee1ae2d05fd90
Dariofelix84 <dariofa69@gmail.com>
2025-12-11 17:08:51 -0300

    Creation RegisterUserRequest

---
+++ segurity/src/main/java/com/dev/felix/segurity/dto/request/RegisterUserRequest.java
@@ -0,0 +1,7 @@
+package com.dev.felix.segurity.dto.request;
+import jakarta.validation.constraints.NotEmpty;
+
+public record RegisterUserRequest(@NotEmpty(message = "Nome é obrigatório") String name,
+                                  @NotEmpty(message = "Email é obrigatório") String email,
+                                  @NotEmpty(message = "Senha é obrigatória") String password) {
+}
