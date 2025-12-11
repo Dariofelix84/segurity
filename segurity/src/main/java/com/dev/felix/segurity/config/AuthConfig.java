72e478932e0005ace2bd7fd746a5998ab867417e
Dariofelix84 <dariofa69@gmail.com>
2025-12-11 17:06:38 -0300

    Implement AuthConfig

---
+++ segurity/src/main/java/com/dev/felix/segurity/config/AuthConfig.java
@@ -0,0 +1,21 @@
+package com.dev.felix.segurity.config;
+
+import com.dev.felix.segurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthConfig implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
